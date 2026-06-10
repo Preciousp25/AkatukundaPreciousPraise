edf841ea81af774293199d4c136b1a3f45315d34
Precious Praise <akatukundapp@gmail.com>
2026-06-10 16:28:41 +0300

    class exercise

--- ClassExamples/Lab2.py
+++ ClassExamples/Lab2.py
@@ -1,23 +1,3 @@
-#score = int(input("Please enter the student's score: "))
-#if score >= 90:
-    #print(" A")
-    #message = "Excellent"
-#elif score >= 80:
-    #print(" B")
-    #message = "Good"
-#elif score >= 70:
-    #print(" C")
-    #message = "Satisfactory"
-#   elif score >= 60:
-    #print(" D")
-    #message = "You need to work harder"
-#else:
-   # print(" F")
-   # message = "Failed"
-#print(f"The student's score is: {score}")
-#print(message)
-
-
  # multiple patterns in one case
 Light_color = input("Please enter the traffic light color (red, yellow, green): ")
 match Light_color:
---
+++ ClassExamples/Lab5.py
@@ -0,0 +1,18 @@
+score = int(input("Please enter the student's score: "))
+if score >= 90:
+    print(" A")
+    message = "Excellent"
+elif score >= 80:
+    print(" B")
+    message = "Good"
+elif score >= 70:
+    print(" C")
+    message = "Satisfactory"
+elif score >= 60:
+    print(" D")
+    message = "You need to work harder"
+else:
+   print(" F")
+   message = "Failed"
+print(f"The student's score is: {score}")
+print(message)
