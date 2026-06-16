9e4791863b4e63960a9b8790145d958f870b8651
Precious Praise <akatukundapp@gmail.com>
2026-06-16 17:01:36 +0300

    added menu- driven calculator

---
+++ Assignment4/Calculator.py
@@ -0,0 +1,51 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
+
+
+while True:
+    print("\nCALCULATOR MENU ")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "5":
+        print("Goodbye!")
+        break
+
+    if choice not in ["1", "2", "3", "4"]:
+        print("Invalid choice. Please select 1-5.")
+        continue
+
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+    except ValueError:
+        print("Invalid input. Numbers only!")
+        continue
+
+    if choice == "1":
+        print("Result =", add(num1, num2))
+
+    elif choice == "2":
+        print("Result =", subtract(num1, num2))
+
+    elif choice == "3":
+        print("Result =", multiply(num1, num2))
+
+    elif choice == "4":
+        print("Result =", divide(num1, num2))
